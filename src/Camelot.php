e5ac3ab4853fa6d5c693e32b1b20ab3967cd14d1
Evan Hildreth <me@eph.me>
2022-05-03 21:46:42 -0400

    Solidify some coding standards

---
+++ src/Camelot.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Smolblog\Core;
+
+/**
+ * Knights, I bid you welcome to your new home.
+ */
+class Camelot {
+	/**
+	 * It's a thing.
+	 *
+	 * @return string What Patsy says.
+	 */
+	public function go() {
+			return 'It\'s only a model.';
+	}
+}
