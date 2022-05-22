bb621b28f416727146e78fb4f31d74dc97837862
Evan Hildreth <me@eph.me>
2022-05-21 22:16:32 -0400

    Define SocialAccount

--- src/Models/SocialAccount.php
+++ src/Models/SocialAccount.php
@@ -2,10 +2,24 @@
 
 namespace Smolblog\Core\Models;
 
+use Smolblog\Core\Environment;
 use Smolblog\Core\Model;
 
 /**
  * Model for storing info about a social media account
  */
 class SocialAccount extends Model {
+	/**
+	 * Available fields for a SocialAccount instance
+	 *
+	 * @var array
+	 */
+	protected array $fields = [
+		'id',
+		'user_id',
+		'social_type',
+		'social_username',
+		'oauth_token',
+		'oauth_secret',
+	];
 }
