8d5b2e97513a9bc8f471172d5bdcfdffb30e25d3
Evan Hildreth <me@eph.me>
2022-05-23 08:55:21 -0400

    Use 'Site' instead of 'Blog'

--- tests/Models/SocialAccountSiteLinkTest.php
+++ tests/Models/SocialAccountSiteLinkTest.php
@@ -8,7 +8,7 @@ use Smolblog\Core\Model;
 use Smolblog\Core\ModelHelper;
 use Smolblog\Core\Exceptions\ModelException;
 
-final class SocialAccountBlogLinkTestHelper implements ModelHelper {
+final class SocialAccountSiteLinkTestHelper implements ModelHelper {
 	public function findAll(string $forModelClass, array $withProperties = []): array {
 		return [
 			new $forModelClass(withHelper: $this, withData: ['id' => 1, ...$withProperties]),
@@ -27,20 +27,20 @@ final class SocialAccountBlogLinkTestHelper implements ModelHelper {
 	}
 }
 
-final class SocialAccountBlogLinkTestEnvironment extends Environment {
+final class SocialAccountSiteLinkTestEnvironment extends Environment {
 	public function getHelperForModel(string $modelClass): ModelHelper {
-		return new SocialAccountBlogLinkTestHelper();
+		return new SocialAccountSiteLinkTestHelper();
 	}
 }
 
 /** @backupStaticAttributes enabled */
-final class SocialAccountBlogLinkTest extends TestCase {
+final class SocialAccountSiteLinkTest extends TestCase {
 	public function testAllDefinedFieldsCanBeAccessed() {
-		$model = new SocialAccountBlogLink(withHelper: new SocialAccountBlogLinkTestHelper());
+		$model = new SocialAccountSiteLink(withHelper: new SocialAccountSiteLinkTestHelper());
 
 		$testData = [
-			'blog_id' => 5,
-			'social_id' => 5,
+			'site_id' => 5,
+			'socialaccount_id' => 5,
 			'additional_info' => 'plausible',
 			'can_push' => true,
 			'can_pull' => true,
@@ -56,20 +56,20 @@ final class SocialAccountBlogLinkTest extends TestCase {
 
 	public function testUndefinedFieldsThrowAnError() {
 		$this->expectNotice();
-		$model = new SocialAccountBlogLink(withHelper: new SocialAccountBlogLinkTestHelper());
+		$model = new SocialAccountSiteLink(withHelper: new SocialAccountSiteLinkTestHelper());
 
 		$model->undefinedField = 'nope';
 	}
 
-	public function testStaticFactoryMethodsReturnSocialAccountBlogLinkModels() {
-		Environment::bootstrap(new SocialAccountBlogLinkTestEnvironment());
+	public function testStaticFactoryMethodsReturnSocialAccountSiteLinkModels() {
+		Environment::bootstrap(new SocialAccountSiteLinkTestEnvironment());
 
-		$this->assertInstanceOf(SocialAccountBlogLink::class, SocialAccountBlogLink::create());
+		$this->assertInstanceOf(SocialAccountSiteLink::class, SocialAccountSiteLink::create());
 
-		$multiple = SocialAccountBlogLink::find(['prop' => 'erty']);
+		$multiple = SocialAccountSiteLink::find(['prop' => 'erty']);
 		$this->assertIsArray($multiple);
 		foreach($multiple as $single) {
-			$this->assertInstanceOf(SocialAccountBlogLink::class, $single);
+			$this->assertInstanceOf(SocialAccountSiteLink::class, $single);
 		}
 	}
 }
