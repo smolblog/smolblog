994fa9c200224729798733846a67f52337d136d6
Evan Hildreth <me@eph.me>
2022-09-17 22:20:49 -0400

    Allow adding vars to Environment

--- src/Environment.php
+++ src/Environment.php
@@ -10,9 +10,21 @@ class Environment {
 	 * Load the information in
 	 *
 	 * @param string $apiBase Base URL (including scheme and trailing slash) for the REST API.
+	 * @param array  $envVars Associative array of arbitrary environment variables.
 	 */
 	public function __construct(
-		public readonly string $apiBase
+		public readonly string $apiBase,
+		private array $envVars = []
 	) {
+	}
+
+	/**
+	 * Quick access for any added environment variables.
+	 *
+	 * @param string $name Environment variable to get.
+	 * @return string
+	 */
+	public function __get(string $name): string {
+		return $this->envVars[$name] ?? null;
 	}
 }
