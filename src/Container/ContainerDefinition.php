02a7352318b7a25e38ba7616df03b8707806f27f
Evan Hildreth <me@eph.me>
2022-09-29 09:13:21 -0400

    ContainerDefinition can only be given a string

--- src/Container/ContainerDefinition.php
+++ src/Container/ContainerDefinition.php
@@ -6,8 +6,8 @@ interface ContainerDefinition {
 	/**
 	 * Add a constructor argument for the given definition.
 	 *
-	 * @param string|callable $arg Class or factory to add to the definition.
+	 * @param string $arg Class or factory to add to the definition.
 	 * @return ContainerDefinition
 	 */
-	public function addArgument(string|callable $arg): ContainerDefinition;
+	public function addArgument(string $arg): ContainerDefinition;
 }
