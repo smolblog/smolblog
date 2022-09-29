b02862560349e7e376825e9bf2e3f30600a84609
Evan Hildreth <me@eph.me>
2022-09-29 09:12:51 -0400

    Correct namespace in test plugin

--- tests/_pluginStub/src/Plugin.php
+++ tests/_pluginStub/src/Plugin.php
@@ -3,7 +3,7 @@
 namespace Smoltest\PluginStub;
 
 use Smolblog\Core\App;
-use Smolblog\Core\Plugin as SmolblogPlugin;
+use Smolblog\Core\Plugin\Plugin as SmolblogPlugin;
 
 class Plugin implements SmolblogPlugin {
 	public function __construct(App $smolblog) {}
