76b61ea9374c9b6e5c6c5a04d6872800103b6ae3
Evan Hildreth <me@eph.me>
2022-10-03 17:48:31 -0400

    Add mapping to CommandBus

--- src/Command/CommandBus.php
+++ src/Command/CommandBus.php
@@ -22,15 +22,24 @@ class CommandBus {
 	private LeagueCommander $internal;
 
 	/**
+	 * Internal Locator class for maintaining command map.
+	 *
+	 * @var ContainerLocator
+	 */
+	private ContainerLocator $locator;
+
+	/**
 	 * Construct the CommandBus
 	 *
-	 * @param array     $map       Array of Command => Handler.
 	 * @param Container $container DI Container with handler classes.
+	 * @param array     $map       Array of Command => Handler.
 	 */
-	public function __construct(array $map, Container $container) {
+	public function __construct(Container $container, array $map = []) {
+		$this->locator = new ContainerLocator($container, $map);
+
 		$commandHandlerMiddleware = new CommandHandlerMiddleware(
 			new ClassNameExtractor(),
-			new ContainerLocator($container, $map),
+			$this->locator,
 			new HandleClassNameInflector(),
 		);
 
@@ -38,6 +47,17 @@ class CommandBus {
 			new LockingMiddleware(),
 			$commandHandlerMiddleware,
 		]);
+	}
+
+	/**
+	 * Map a command to a handler.
+	 *
+	 * @param string $command Fully-qualified class name of the command.
+	 * @param string $handler Fully-qualified class name of the handler.
+	 * @return void
+	 */
+	public function map(string $command, string $handler): void {
+		$this->locator->addHandler($handler, $command);
 	}
 
 	/**
