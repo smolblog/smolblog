140f8b7c1b1e4a1c262c1b6e85c402fa8f2bae6a
Evan Hildreth <me@eph.me>
2022-10-14 20:06:15 -0400

    Adding test

--- tests/Entity/EntityTest.php
+++ tests/Entity/EntityTest.php
@@ -32,4 +32,25 @@ final class EntityTest extends TestCase {
 		$this->assertInstanceOf(Entity::class, $test);
 		$this->assertEquals('Luke', $test->id);
 	}
+
+	public function testTwoEntitiesWithTheSameClassAndIdHaveEqualStringRepresentation() {
+		$test1 = new EntityTestConstructor(id: 42, name: 'Arthur');
+		$test2 = new EntityTestConstructor(id: 42, name: 'Dent');
+
+		$this->assertEquals(strval($test1), strval($test2));
+	}
+
+	public function testTwoEntitiesWithDifferentClassesHaveDifferentStringRepresentation() {
+		$test1 = new EntityTestConstructor(id: 'Arthur', name: 'Arthur');
+		$test2 = new EntityTestHidden(name: 'Arthur');
+
+		$this->assertNotEquals(strval($test1), strval($test2));
+	}
+
+	public function testTwoEntitiesWithDifferentIdsHaveDifferentStringRepresentation() {
+		$test1 = new EntityTestHidden(name: 'Arthur');
+		$test2 = new EntityTestHidden(name: 'Dent');
+
+		$this->assertNotEquals(strval($test1), strval($test2));
+	}
 }
