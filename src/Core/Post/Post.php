5965f71fdbae6e0508ce6e4a3610eeba9225e227
Evan Hildreth <me@eph.me>
2022-12-07 21:52:42 -0500

    Allow Post to use any DateTimeInterface

--- src/Core/Post/Post.php
+++ src/Core/Post/Post.php
@@ -2,7 +2,7 @@
 
 namespace Smolblog\Core\Post;
 
-use DateTime;
+use DateTimeInterface;
 use Smolblog\Framework\Entity;
 use Smolblog\Framework\Identifier;
 
@@ -13,17 +13,17 @@ class Post extends Entity {
 	/**
 	 * Create the Post object
 	 *
-	 * @param integer    $user_id   ID of the Post's author.
-	 * @param DateTime   $timestamp Time and Date for post: time last saved if draft, time published if not.
-	 * @param string     $slug      URL-friendly identifier for the post.
-	 * @param Identifier $id        Unique ID of the post in the blog. Creates date-based ID if not provided.
-	 * @param string     $title     Title of the post.
-	 * @param Block[]    $content   Post's content as an ordered array of blocks.
-	 * @param PostStatus $status    Status of the post.
+	 * @param integer           $user_id   ID of the Post's author.
+	 * @param DateTimeInterface $timestamp Time and Date for post: time last saved if draft, time published if not.
+	 * @param string            $slug      URL-friendly identifier for the post.
+	 * @param Identifier        $id        Unique ID of the post in the blog. Creates date-based ID if not provided.
+	 * @param string            $title     Title of the post.
+	 * @param Block[]           $content   Post's content as an ordered array of blocks.
+	 * @param PostStatus        $status    Status of the post.
 	 */
 	public function __construct(
 		public readonly int $user_id,
-		public readonly DateTime $timestamp,
+		public readonly DateTimeInterface $timestamp,
 		public readonly string $slug,
 		Identifier $id = null,
 		public readonly ?string $title = null,
