6d946d16ba557d227f0b72adce89a0aa817e4505
Evan Hildreth <me@eph.me>
2022-12-12 21:30:54 -0500

    Add additional post fields

--- src/Core/Post/Post.php
+++ src/Core/Post/Post.php
@@ -14,13 +14,17 @@ class Post extends Entity {
 	/**
 	 * Create the Post object
 	 *
-	 * @param integer           $user_id   ID of the Post's author.
-	 * @param DateTimeInterface $timestamp Time and Date for post: time last saved if draft, time published if not.
-	 * @param string            $slug      URL-friendly identifier for the post.
-	 * @param Identifier        $id        Unique ID of the post in the blog. Creates date-based ID if not provided.
-	 * @param string            $title     Title of the post.
-	 * @param Block[]           $content   Post's content as an ordered array of blocks.
-	 * @param PostStatus        $status    Status of the post.
+	 * @param integer           $user_id         ID of the Post's author.
+	 * @param DateTimeInterface $timestamp       Time and Date post was/will be published.
+	 * @param string            $slug            URL-friendly identifier for the post.
+	 * @param Identifier        $id              Unique ID of the post in the blog. Creates date-based ID if needed.
+	 * @param string            $title           Title of the post.
+	 * @param Block[]           $content         Post's content as an ordered array of blocks.
+	 * @param PostStatus        $status          Status of the post.
+	 * @param string[]          $tags            Freeform tags for the post.
+	 * @param string            $reblog          URL this post was reblogged from; null for original content.
+	 * @param string[]          $syndicationUrls External URLs this post this post can be viewed at.
+	 * @param mixed[]           $meta            Unstructured metadata.
 	 */
 	public function __construct(
 		public readonly int $user_id,
@@ -30,6 +34,10 @@ class Post extends Entity {
 		public readonly ?string $title = null,
 		public readonly array $content = [],
 		public readonly PostStatus $status = PostStatus::Draft,
+		public readonly array $tags = [],
+		public readonly ?string $reblog = null,
+		public readonly array $syndicationUrls = [],
+		public readonly array $meta = [],
 	) {
 		parent::__construct(id: $id ?? Identifier::createFromDate());
 	}
@@ -44,7 +52,8 @@ class Post extends Entity {
 		$arr['timestamp'] = $this->timestamp->format(DateTimeInterface::RFC3339_EXTENDED);
 		$arr['content'] = array_map(fn($block) => $block->toArray(), $this->content);
 
-		return $arr;
+		// Filter out null values to decrease noise.
+		return array_filter($arr);
 	}
 
 	/**
