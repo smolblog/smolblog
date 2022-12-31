6d3e72432c2a2206b05b1db592e59a402dec77e2
Evan Hildreth <me@eph.me>
2022-12-31 14:30:27 -0500

    Add RegistrarKit

---
+++ tests/Framework/Objects/RegistrarKitTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Smolblog\Framework\Objects;
+
+use PHPUnit\Framework\TestCase;
+use Psr\Container\ContainerInterface;
+
+interface MockRegisterable {}
+
+class RegisterableMock implements MockRegisterable {
+	public readonly string $id;
+
+	public function __construct() {
+		$this->id = uniqid();
+	}
+}
+
+class ConcreteRegistrar {
+	use RegistrarKit;
+
+	function __construct(ContainerInterface $container) {
+		$this->container = $container;
+		$this->interface = MockRegisterable::class;
+	}
+}
+
+final class RegistrarKitTest extends TestCase {
+	public function testRegisterableObjectCanBeRegisteredAndRetrieved() {
+		$expected = new RegisterableMock();
+
+		$container = $this->createStub(ContainerInterface::class);
+		$container->method('has')->willReturn(true);
+		$container->method('get')->willReturn($expected);
+
+		$registrar = new ConcreteRegistrar(container: $container);
+		$registrar->register(key: 'one', class: RegisterableMock::class);
+
+		$this->assertTrue($registrar->has('one'));
+
+		$actual = $registrar->get('one');
+		$this->assertEquals(
+			$expected->id,
+			$actual->id
+		);
+	}
+
+	public function testWillNotSaveWhenClassNotRegisterable() {
+		$registrar = new ConcreteRegistrar(container: $this->createStub(ContainerInterface::class));
+		$registrar->register(key: 'nope', class: self::class);
+
+		$this->assertFalse($registrar->has('nope'));
+	}
+
+	public function testRegistrarGivesNullWhenNotRegistered() {
+		$registrar = new ConcreteRegistrar(container: $this->createStub(ContainerInterface::class));
+
+		$this->assertFalse($registrar->has('nope'));
+		$this->assertNull($registrar->get('nope'));
+	}
+
+	public function testRegistrarGivesNullWhenNotInContainer() {
+		$container = $this->createStub(ContainerInterface::class);
+		$container->method('has')->willReturn(false);
+
+		$registrar = new ConcreteRegistrar($container);
+		$registrar->register(key: 'maybe', class: RegisterableMock::class);
+
+		$this->assertFalse($registrar->has('maybe'));
+		$this->assertNull($registrar->get('maybe'));
+	}
+}
