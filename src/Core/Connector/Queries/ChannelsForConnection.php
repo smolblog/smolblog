b640b9beecd3bef78760a64e778d1a18090f422c
Evan Hildreth <me@eph.me>
2022-12-31 23:08:25 -0500

    Connector Queries

---
+++ src/Core/Connector/Queries/ChannelsForConnection.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Smolblog\Core\Connector\Queries;
+
+use Smolblog\Framework\Messages\Query;
+use Smolblog\Framework\Objects\Identifier;
+
+/**
+ * Find all Channels for a Connection. Returns an array of Channel objects.
+ *
+ * If the Connection's provider and provider key are known, the Connection ID can be created using the static
+ * Smolblog\Core\Connector\Entities\Connection::buildId function.
+ */
+class ChannelsForConnection extends Query {
+	/**
+	 * Construct the query
+	 *
+	 * @param Identifier $connectionId ID of the Connection to fetch Channels for.
+	 */
+	public function __construct(
+		public readonly Identifier $connectionId,
+	) {
+	}
+}
--- src/Core/Connector/Queries/ConnectionsForUser.php
+++ src/Core/Connector/Queries/ConnectionsForUser.php
@@ -6,19 +6,19 @@ use Smolblog\Framework\Messages\Query;
 use Smolblog\Framework\Objects\Identifier;
 
 /**
- * Find a Channel via its ID; returns a single Channel object.
+ * Find the Connections assigned to a User and fetch them. Returns an array of Connection objects.
  *
  * If the Connection ID and channel Key are known, the Channel ID can be created using the static
  * Smolblog\Core\Connector\Entities\Channel::buildId function.
  */
-class ChannelById extends Query {
+class ConnectionsForUser extends Query {
 	/**
 	 * Construct the query
 	 *
-	 * @param Identifier $channelId ID of the channel to fetch.
+	 * @param Identifier $userId ID of the user whose Connections to fetch.
 	 */
 	public function __construct(
-		public readonly Identifier $channelId,
+		public readonly Identifier $userId,
 	) {
 	}
 }
