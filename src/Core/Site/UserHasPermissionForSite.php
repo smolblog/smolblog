cfca0bc5cf94b4b23a76d9a2ece9e508eace586e
Evan Hildreth <me@eph.me>
2023-01-21 19:24:30 -0500

    Write some queries

---
+++ src/Core/Site/UserHasPermissionForSite.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Smolblog\Core\Site;
+
+use Smolblog\Framework\Messages\Query;
+use Smolblog\Framework\Objects\Identifier;
+
+/**
+ * Check if a given user can perform a given action for a given site.
+ */
+class UserHasPermissionForSite extends Query {
+	/**
+	 * Construct the query.
+	 *
+	 * @param Identifier $siteId       ID of the site in question.
+	 * @param Identifier $userId       ID of the user in question.
+	 * @param boolean    $mustBeAuthor True if the user must have Author-level permissions.
+	 * @param boolean    $mustBeAdmin  True if the user must have Admin-level permissions.
+	 */
+	public function __construct(
+		public readonly Identifier $siteId,
+		public readonly Identifier $userId,
+		public readonly bool $mustBeAuthor,
+		public readonly bool $mustBeAdmin,
+	) {
+	}
+}
