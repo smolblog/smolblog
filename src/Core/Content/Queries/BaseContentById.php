f000fa9c318fa2ad77f3d342016d9be7658fc823
Evan Hildreth <me@eph.me>
2023-03-31 20:52:51 -0400

    Set up content query security

---
+++ src/Core/Content/Queries/BaseContentById.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Smolblog\Core\Content\Queries;
+
+use Smolblog\Framework\Messages\AuthorizableMessage;
+use Smolblog\Framework\Messages\MemoizableQuery;
+use Smolblog\Framework\Messages\MemoizableQueryKit;
+use Smolblog\Framework\Messages\Query;
+use Smolblog\Framework\Messages\StoppableMessageKit;
+use Smolblog\Framework\Objects\Identifier;
+
+/**
+ * Base class for singluar content queries.
+ *
+ * Sets up memoization, content extensions, and a visibility security check.
+ *
+ * Use GenericContentById for a concrete query.
+ */
+abstract class BaseContentById extends Query implements MemoizableQuery, ExtensableContentQuery, AuthorizableMessage {
+	use MemoizableQueryKit;
+	use ExtensableContentQueryKit;
+	use StoppableMessageKit;
+
+	/**
+	 * Construct the query.
+	 *
+	 * @param Identifier      $id     ID of the content being queried.
+	 * @param Identifier|null $userId Optional user making the request.
+	 */
+	public function __construct(
+		public readonly Identifier $id,
+		public readonly ?Identifier $userId = null
+	) {
+	}
+
+	/**
+	 * Get the authorization query and check if the given user can see the given content.
+	 *
+	 * @return Query
+	 */
+	public function getAuthorizationQuery(): Query {
+		return new ContentVisibleToUser(contentId: $this->id, userId: $this->userId);
+	}
+}
