7c31b2e0c0f588482c89546ea8c7911183578ed7
Evan Hildreth <me@eph.me>
2023-04-05 22:52:48 -0400

    List content endpoints more reasonably

--- src/Api/Model.php
+++ src/Api/Model.php
@@ -43,12 +43,12 @@ class Model extends DomainModel {
 
 		Content\ListContent::class => ['bus' => MessageBus::class],
 		Content\GetReblog::class => ['bus' => MessageBus::class],
-		Content\GetStatus::class => ['bus' => MessageBus::class],
 		Content\CreateReblog::class => ['bus' => MessageBus::class],
-		Content\CreateStatus::class => ['bus' => MessageBus::class],
 		Content\UpdateReblog::class => ['bus' => MessageBus::class],
-		Content\UpdateStatus::class => ['bus' => MessageBus::class],
 		Content\DeleteReblog::class => ['bus' => MessageBus::class],
+		Content\GetStatus::class => ['bus' => MessageBus::class],
+		Content\CreateStatus::class => ['bus' => MessageBus::class],
+		Content\UpdateStatus::class => ['bus' => MessageBus::class],
 		Content\DeleteStatus::class => ['bus' => MessageBus::class],
 
 		Preview\PreviewEmbed::class => ['embed' => ExternalContentService::class],
