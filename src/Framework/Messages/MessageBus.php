b5ce8c61f64d1a2f3c37f43ac3cf4dc9fdfd8a85
Evan Hildreth <me@eph.me>
2023-05-06 22:04:26 -0400

    Async support

--- src/Framework/Messages/MessageBus.php
+++ src/Framework/Messages/MessageBus.php
@@ -15,10 +15,10 @@ interface MessageBus extends EventDispatcherInterface {
 	/**
 	 * Dispatch the given message to its listeners.
 	 *
-	 * @param mixed $message Message to send.
+	 * @param Message $message Message to send.
 	 * @return mixed Message potentially modified by listeners.
 	 */
-	public function dispatch(mixed $message): mixed;
+	public function dispatch(Message $message): mixed;
 
 	/**
 	 * Convenience method for sending Query messages that will return the results.
@@ -28,6 +28,17 @@ interface MessageBus extends EventDispatcherInterface {
 	 */
 	public function fetch(Query $query): mixed;
 
+	/**
+	 * Dispatch the given message on a separate thread.
+	 *
+	 * No guidance is given where said thread is. It could be a queued job on the same thread, it could be a managed
+	 * queue, it could be an entirely different server. As such, the given Message should have as much information
+	 * included as reasonably possible.
+	 *
+	 * @param Message $message Message to send.
+	 * @return void
+	 */
+	public function dispatchAsync(Message $message): void;
+
 	// TODO: regenerate(Event): method that takes an Event and only calls Projection services.
-	// TODO: async(mixed): method that dispatches a message on a separate thread.
 }
