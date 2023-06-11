da2e501c8d576ae44850596862f70fe0ff44d180
Evan Hildreth <me@eph.me>
2023-06-11 13:43:04 -0400

    Add todo note

--- tests/Framework/Infrastructure/HttpSignerTest.php
+++ tests/Framework/Infrastructure/HttpSignerTest.php
@@ -79,6 +79,7 @@ final class HttpSignerTest extends TestCase {
 		$service = new HttpSigner();
 		$request = $service->sign(request: $request, keyId: 'key', keyPem: self::PRIVATE_KEY);
 
+		// TODO: This test can fail if the previous line takes place in the previous second.
 		$this->assertEquals(date(DateTimeInterface::RFC7231), $request->getHeaderLine('Date'));
 	}
 }
