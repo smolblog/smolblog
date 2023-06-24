a10bfb37ddf8585304d4f351e8fd46c0ff3bbc7b
Evan Hildreth <me@eph.me>
2023-06-23 21:41:15 -0400

    Add follower query

---
+++ src/Core/Federation/GetFollowersForSite.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Smolblog\Core\Federation;
+
+use Smolblog\Framework\Messages\MemoizableQuery;
+use Smolblog\Framework\Objects\Identifier;
+
+/**
+ * Get the array of followers for the given site.
+ */
+class GetFollowersForSite extends MemoizableQuery {
+	/**
+	 * Construct the query.
+	 *
+	 * @param Identifier $siteId Site to query.
+	 */
+	public function __construct(
+		public readonly Identifier $siteId,
+	) {
+	}
+
+	/**
+	 * Get the results.
+	 *
+	 * @return Follower[]
+	 */
+	public function results(): array {
+		return $this->results;
+	}
+}
