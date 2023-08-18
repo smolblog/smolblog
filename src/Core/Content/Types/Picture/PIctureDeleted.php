d8e7793b79780058740e4fd0424b1dbf73626f3f
Evan Hildreth <me@eph.me>
2023-08-17 21:53:04 -0400

    Simple commit to trigger redeploy.

--- src/Core/Content/Types/Picture/PIctureDeleted.php
+++ src/Core/Content/Types/Picture/PIctureDeleted.php
@@ -8,7 +8,7 @@ use Smolblog\Core\Content\Events\ContentDeleted;
  * Indicates that a Picture has been deleted.
  *
  * There is no extra information attached to this event, but it is its own event so it can be picked up by the
- * PictureProjection (without it having to listen to *every* ContentDeleted event.)
+ * PictureProjection (without it having to listen to *all* ContentDeleted events.)
  */
 class PictureDeleted extends ContentDeleted {
 }
