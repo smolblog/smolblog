db3759d75b7cf3dcd0cc6d6d9ba7c0c0bf6c5a90
Evan Hildreth <me@eph.me>
2023-09-09 12:51:34 -0400

    Update Model

--- src/Tumblr/Model.php
+++ src/Tumblr/Model.php
@@ -5,5 +5,24 @@ namespace Smolblog\Tumblr;
 use Smolblog\Framework\Objects\DomainModel;
 
 class Model extends DomainModel {
-	public const SERVICES = [];
+	public const SERVICES = [
+		TumblrConnector::class => [
+			'factory' => TumblrClientFactory::class,
+		],
+	];
+
+	public static function getDependencyMap(): array
+	{
+		if (!defined('SMOLBLOG_TUMBLR_APPLICATION_KEY')) {
+			return self::SERVICES;
+		}
+
+		return [
+			...self::SERVICES,
+			TumblrClientFactory::class => [
+				'appKey' => fn() => SMOLBLOG_TUMBLR_APPLICATION_KEY,
+				'appSecret' => fn() => SMOLBLOG_TUMBLR_APPLICATION_SECRET,
+			],
+		];
+	}
 }
