8f08fcd7cf172c16bdf3be18995fb03d486e1353
Evan Hildreth <me@eph.me>
2024-01-20 14:45:59 -0500

    Update and merge composer definitions

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -2,8 +2,20 @@
 
 declare(strict_types=1);
 
+use Symplify\MonorepoBuilder\ComposerJsonManipulator\ValueObject\ComposerJsonSection;
 use Symplify\MonorepoBuilder\Config\MBConfig;
 
 return static function (MBConfig $mbConfig): void {
-    $mbConfig->packageDirectories([__DIR__ . '/packages']);
+	$mbConfig->packageDirectories([
+		__DIR__ . '/packages/framework',
+		__DIR__ . '/packages/product',
+	]);
+
+	$mbConfig->dataToAppend([
+		ComposerJsonSection::AUTOLOAD_DEV => [
+			'psr-4' => [
+				'Smolblog\\Mock\\' => 'mock-app/'
+			],
+		],
+	]);
 };
