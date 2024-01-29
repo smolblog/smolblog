560d3238941488d468c2ea34d8293c4dd3d7a12c
Evan Hildreth <me@eph.me>
2024-01-28 19:52:15 -0500

    Draft registration page changes

--- application/wordpress/src/Plugin.php
+++ application/wordpress/src/Plugin.php
@@ -23,5 +23,22 @@ class Plugin {
 		);
 
 		add_action( 'rest_api_init', fn() => $app->container->get(EndpointRegistrar::class)->init() );
+
+		add_action( 'signup_extra_fields', function() {
+			?>
+			<label for="subscription_key">Subscription Key:</label>
+			<input name="subscription_key" type="text" id="subscription_key" value="" autocapitalize="none" autocorrect="off" maxlength="60" required="required" aria-describedby="wp-signup-subkey-description">
+			<p id="wp-signup-subkey-description">Provide the access key you received.</p>
+			<?php
+		}, 10, 0 );
+
+		add_filter( 'registration_errors', function($errors, $sanitized_user_login, $user_email) {
+			if (!isset($_POST['subscription_key'])) {
+				$errors->add( 'no_sub_key', 'Smolblog is in private beta; a subscription key is required.' );
+			}
+			if ($_POST['subscription_key'] !== '**something**') {
+				$errors->add( 'bad_sub_key', 'Could not validate subscription key.' );
+			}
+		}, 10, 3);
 	}
 }
