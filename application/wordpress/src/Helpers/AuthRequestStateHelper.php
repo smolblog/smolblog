20d3d276a1b06c17bf79b2848c01d74a148ed03b
Evan Hildreth <me@eph.me>
2024-10-01 15:57:31 -0400

    Disable API tests for now

--- application/wordpress/src/Helpers/AuthRequestStateHelper.php
+++ application/wordpress/src/Helpers/AuthRequestStateHelper.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Attach the Smolblog AuthRequestState model to WordPress' transient funcitons.
  *
@@ -9,27 +10,26 @@
 
 namespace Smolblog\WP\Helpers;
 
-use Smolblog\Core\Connector\Entities\AuthRequestState;
-use Smolblog\Core\Connector\Services\AuthRequestStateRepo;
+use Smolblog\Core\Connection\Data\AuthRequestStateRepo;
+use Smolblog\Core\Connection\Entities\AuthRequestState;
 
 /**
  * Helper class to link WordPress and Smolblog transients.
  */
 class AuthRequestStateHelper implements AuthRequestStateRepo {
-
 	/**
 	 * Get the indicated AuthRequestState from the repository. Should return null if not found.
 	 *
-	 * @param string $id Unique identifier for the object.
+	 * @param string $key Unique identifier for the object.
 	 * @return AuthRequestState State identified by $id; null if it does not exist.
 	 */
-	public function getAuthRequestState( string $key ): AuthRequestState {
-		$state = get_transient( $key );
-		if ( ! is_array( $state ) ) {
+	public function getAuthRequestState(string $key): ?AuthRequestState {
+		$state = get_transient($key);
+		if (! is_array($state)) {
 			return null;
 		}
 
-		return AuthRequestState::fromArray($state);
+		return AuthRequestState::deserializeValue($state);
 	}
 
 	/**
@@ -38,7 +38,7 @@ class AuthRequestStateHelper implements AuthRequestStateRepo {
 	 * @param AuthRequestState $state State to save.
 	 * @return void
 	 */
-	public function saveAuthRequestState( AuthRequestState $state ): void {
-		set_transient( $state->key, $state->toArray(), 60 * 15 );
+	public function saveAuthRequestState(AuthRequestState $state): void {
+		set_transient($state->key, $state->serializeValue(), 60 * 15);
 	}
 }
