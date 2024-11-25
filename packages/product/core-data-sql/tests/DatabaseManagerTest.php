b9b149562270bc6bb04e151142bb6fac27583765
Evan Hildreth <me@eph.me>
2024-11-24 19:14:51 -0500

    Test schema migration

--- packages/product/core-data-sql/tests/DatabaseManagerTest.php
+++ packages/product/core-data-sql/tests/DatabaseManagerTest.php
@@ -2,6 +2,7 @@
 
 namespace Smolblog\CoreDataSql;
 
+use Doctrine\DBAL\Schema\Schema;
 use Exception;
 use Smolblog\CoreDataSql\Test\DataTestBase;
 use Smolblog\CoreDataSql\Test\TestDatabaseManager;
@@ -35,5 +36,55 @@ final class DatabaseManagerTest extends DataTestBase {
 
 		$service->testSetSchemaVersion('testversion');
 		$this->assertEquals('testversion', $service->testGetSchemaVersion());
+	}
+
+	public function testSchemaWillMigrate() {
+		$service = $this->app->container->get(TestDatabaseManager::class);
+		$db = $service->getConnection();
+		$testConfig = [
+			ContentProjection::class,
+			EventStream::class,
+		];
+
+		$this->assertNotNull($service->testGetSchemaVersion());
+
+		$firstProjection = new class implements DatabaseTableHandler {
+			public static function addTableToSchema(Schema $schema): Schema {
+				$table = $schema->createTable('test_projection');
+				$table->addColumn('dbid', 'integer', ['unsigned' => true, 'autoincrement' => true]);
+				$table->addColumn('first_uuid', 'guid');
+				$table->setPrimaryKey(['dbid']);
+				return $schema;
+			}
+		};
+		$service->configure([
+			...$testConfig,
+			get_class($firstProjection),
+		]);
+		$db->insert('test_projection', ['first_uuid' => '0d1d4377-8003-4b15-a189-028cb93013df']);
+
+		$secondProjection = new class implements DatabaseTableHandler {
+			public static function addTableToSchema(Schema $schema): Schema {
+				$table = $schema->createTable('test_projection');
+				$table->addColumn('dbid', 'integer', ['unsigned' => true, 'autoincrement' => true]);
+				$table->addColumn('first_uuid', 'guid');
+				$table->addColumn('second_uuid', 'guid', ['notnull' => false]);
+				$table->setPrimaryKey(['dbid']);
+				return $schema;
+			}
+		};
+		$service->configure([
+			...$testConfig,
+			get_class($secondProjection),
+		]);
+		$db->insert('test_projection', [
+			'first_uuid' => '0da5eef8-573b-4437-851f-08db2c98dd74',
+			'first_uuid' => 'ce86d81f-0dc7-4721-a3aa-9b88562e662f',
+		]);
+
+		$this->assertEquals(
+			'0d1d4377-8003-4b15-a189-028cb93013df',
+			$db->fetchOne('SELECT first_uuid FROM test_projection WHERE second_uuid IS NULL'),
+		);
 	}
 }
