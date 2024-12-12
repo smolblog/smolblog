14157157b66f301db2859275296de31c3f1396b0
Evan Hildreth <me@eph.me>
2024-12-12 14:24:37 -0500

    Add auto service registration

--- packages/framework/foundation/src/DomainModel.php
+++ packages/framework/foundation/src/DomainModel.php
@@ -2,18 +2,69 @@
 
 namespace Smolblog\Foundation;
 
+use ReflectionClass;
+use ReflectionNamedType;
+use Smolblog\Foundation\Exceptions\CodePathNotSupported;
+
 /**
  * Class to centralize services (with dependencies) for a domain.
  */
 abstract class DomainModel {
+	public const AUTO_SERVICES = [];
 	public const SERVICES = [];
 
 	/**
 	 * Get services defined in this model and the dependencies they need.
 	 *
-	 * @return array<string, array<string, mixed>>
+	 * @return array<class-string, array<string, mixed>>
 	 */
 	public static function getDependencyMap(): array {
-		return static::SERVICES;
+		return [
+			...self::autoregisterServices(),
+			...static::SERVICES,
+		];
+	}
+
+	/**
+	 * Get services marked for auto-registration, reflect their constructors, and create the dependency map.
+	 *
+	 * @return array<class-string, array<string, mixed>>
+	 */
+	private static function autoregisterServices(): array {
+		$deps = [];
+		foreach (static::AUTO_SERVICES as $service) {
+			$deps[$service] = self::reflectService($service);
+		}
+		return $deps;
+	}
+
+	/**
+	 * Reflect the given service and return its dependency array.
+	 *
+	 * @throws CodePathNotSupported When service's constructor takes untyped or union/intersection typed arguments.
+	 *
+	 * @param class-string $service Service to reflect.
+	 * @return array<string, mixed>
+	 */
+	private static function reflectService(string $service): array {
+		$reflect = (new ReflectionClass($service))->getConstructor();
+		if (!isset($reflect)) {
+			return [];
+		}
+
+		$params = [];
+		foreach ($reflect->getParameters() as $param) {
+			$type = $param->getType();
+			if (!isset($type) || get_class($type) !== ReflectionNamedType::class || $type->isBuiltin()) {
+				throw new CodePathNotSupported(
+					message: "{$service} cannot be auto-registered; parameter {$param->getName()} is not a class.",
+					location: static::class . '::AUTO_SERVICES',
+				);
+			}
+
+			$params[$param->getName()] = $type->getName();
+		}
+
+		return $params;
 	}
 }
--- packages/product/core/src/Model.php
+++ packages/product/core/src/Model.php
@@ -2,109 +2,69 @@
 
 namespace Smolblog\Core;
 
-use Illuminate\Database\ConnectionInterface;
 use Psr\Container\ContainerInterface;
-use Psr\EventDispatcher\EventDispatcherInterface;
-use Smolblog\Framework\Messages\MessageBus;
 use Smolblog\Foundation\DomainModel;
-use Smolblog\Foundation\Service\KeypairGenerator;
-use Smolblog\Markdown\SmolblogMarkdown;
 
 /**
  * Set up the services and listeners for the Core domain model.
  */
 class Model extends DomainModel {
+	public const AUTO_COMMANDS = [
+		Channel\Commands\AddChannelToSite::class,
+		Channel\Commands\PushContentToChannel::class,
+
+		Connection\Commands\BeginAuthRequest::class,
+		Connection\Commands\DeleteConnection::class,
+		Connection\Commands\FinishAuthRequest::class,
+		Connection\Commands\RefreshChannels::class,
+		Connection\Commands\RefreshConnection::class,
+
+		Content\Commands\CreateContent::class,
+		Content\Commands\DeleteContent::class,
+		Content\Commands\UpdateContent::class,
+
+		Media\Commands\DeleteMedia::class,
+		Media\Commands\EditMediaAttributes::class,
+		Media\Commands\HandleUploadedMedia::class,
+		Media\Commands\SideloadMedia::class,
+
+		Site\Commands\CreateSite::class,
+		Site\Commands\SetUserSitePermissions::class,
+		Site\Commands\UpdateSiteDetails::class,
+	];
+
+	public const AUTO_SERVICES = [
+		Channel\Services\ChannelHandlerRegistry::class,
+		Channel\Services\ChannelLinker::class,
+		Channel\Services\ContentPushService::class,
+		Channel\Services\ChannelQueryService::class,
+
+		Connection\Services\AuthRequestService::class,
+		Connection\Services\ConnectionChannelRefresher::class,
+		Connection\Services\ConnectionHandlerRegistry::class,
+		Connection\Services\ConnectionRefresher::class,
+		Connection\Services\ConnectionDeletionService::class,
+
+		Content\Services\ContentExtensionRegistry::class,
+		Content\Services\ContentService::class,
+		Content\Services\ContentTypeRegistry::class,
+
+		Content\Types\Article\ArticleService::class,
+		Content\Types\Note\NoteService::class,
+		Content\Types\Picture\PictureService::class,
+		Content\Types\Reblog\ReblogService::class,
+
+		Content\Extensions\Tags\TagsService::class,
+		Content\Extensions\Warnings\WarningsService::class,
+
+		Media\Services\MediaService::class,
+
+		Site\Services\SiteService::class,
+	];
+
 	public const SERVICES = [
-		Channel\Services\ChannelHandlerRegistry::class => [
-			'container' => ContainerInterface::class,
-		],
-		Channel\Services\ChannelLinker::class => [
-			'channels' => Channel\Data\ChannelRepo::class,
-			'eventBus' => EventDispatcherInterface::class,
-			'perms' => Permissions\SitePermissionsService::class,
-		],
-		Channel\Services\ContentPushService::class => [
-			'eventBus' => EventDispatcherInterface::class,
-			'perms' => Permissions\SitePermissionsService::class,
-			'contentRepo' => Content\Data\ContentRepo::class,
-			'channelRepo' => Channel\Data\ChannelRepo::class,
-			'handlers' => Channel\Services\ChannelHandlerRegistry::class,
-		],
-		Channel\Services\ChannelQueryService::class => [
-			'repo' => Channel\Data\ChannelRepo::class,
-			'sitePerms' => Permissions\SitePermissionsService::class,
-		],
-
-		Connection\Services\AuthRequestService::class => [
-			'handlers' => Connection\Services\ConnectionHandlerRegistry::class,
-			'stateRepo' => Connection\Data\AuthRequestStateRepo::class,
-			'eventBus' => EventDispatcherInterface::class,
-			'refresher' => Connection\Services\ConnectionChannelRefresher::class,
-		],
-		Connection\Services\ConnectionChannelRefresher::class => [
-			'connections' => Connection\Data\ConnectionRepo::class,
-			'channels' => Channel\Data\ChannelRepo::class,
-			'handlers' => Connection\Services\ConnectionHandlerRegistry::class,
-			'eventBus' => EventDispatcherInterface::class,
-		],
-		Connection\Services\ConnectionHandlerRegistry::class => [
-			'container' => ContainerInterface::class,
-		],
-		Connection\Services\ConnectionRefresher::class => [
-			'connections' => Connection\Data\ConnectionRepo::class,
-			'handlers' => Connection\Services\ConnectionHandlerRegistry::class,
-			'eventBus' => EventDispatcherInterface::class,
-		],
-		Connection\Services\ConnectionDeletionService::class => [
-			'connections' => Connection\Data\ConnectionRepo::class,
-			'eventBus' => EventDispatcherInterface::class,
-		],
-
-		Content\Services\ContentExtensionRegistry::class => [
-			'container' => ContainerInterface::class,
-		],
-		Content\Services\ContentService::class => [
-			'types' => Content\Services\ContentTypeRegistry::class,
-			'extensions' => Content\Services\ContentExtensionRegistry::class,
-			'repo' => Content\Data\ContentRepo::class,
-			'perms' => Permissions\SitePermissionsService::class,
-		],
-		Content\Services\ContentTypeRegistry::class => [
-			'container' => ContainerInterface::class,
-		],
-
-		Content\Types\Article\ArticleService::class => [
-			'eventBus' => EventDispatcherInterface::class,
-		],
-		Content\Types\Note\NoteService::class => [
-			'eventBus' => EventDispatcherInterface::class,
-		],
-		Content\Types\Picture\PictureService::class => [
-			'eventBus' => EventDispatcherInterface::class,
-		],
-		Content\Types\Reblog\ReblogService::class => [
-			'eventBus' => EventDispatcherInterface::class,
-		],
-
-		Content\Extensions\Tags\TagsService::class => [],
-		Content\Extensions\Warnings\WarningsService::class => [],
-
 		Media\Services\MediaHandlerRegistry::class => [
-			'container' => ContainerInterface::class,
+		'container' => ContainerInterface::class,
 		],
-		Media\Services\MediaService::class => [
-			'bus' => EventDispatcherInterface::class,
-			'registry' => Media\Services\MediaHandlerRegistry::class,
-			'mediaRepo' => Media\Data\MediaRepo::class,
-			'perms' => Permissions\SitePermissionsService::class,
-		],
-
-		Site\Services\SiteService::class => [
-			'globalPerms' => Permissions\GlobalPermissionsService::class,
-			'sitePerms' => Permissions\SitePermissionsService::class,
-			'repo' => Site\Data\SiteRepo::class,
-			'keygen' => KeypairGenerator::class,
-			'eventBus' => EventDispatcherInterface::class,
-		]
 	];
 }
