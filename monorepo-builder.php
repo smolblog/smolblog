3bdcfbb423a8dbedd0e38179e2068f19953913b1
Evan Hildreth <me@eph.me>
2025-02-01 20:16:34 -0500

    Remove unused packages

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -11,5 +11,21 @@ return static function (MBConfig $mbConfig): void {
 		__DIR__ . '/packages/product',
 	]);
 
-	$mbConfig->dataToAppend([]);
+	$mbConfig->dataToAppend([
+		'require-dev' => [
+			'symplify/monorepo-builder' => '^11.2',
+		],
+		"scripts" => [
+			"test" => "phpunit --testdox --no-coverage",
+			"test-fast" => "phpunit --testsuite unit --no-coverage",
+			"test-coverage" => [
+				"@putenv XDEBUG_MODE=coverage",
+				"phpunit --testsuite unit",
+				"Smolblog\\Test\\CoverageReport::report"
+			],
+			"lint" => "./vendor/squizlabs/php_codesniffer/bin/phpcs",
+			"lintfix" => "./vendor/squizlabs/php_codesniffer/bin/phpcbf",
+			"monorepo" => "./vendor/bin/monorepo-builder",
+		]
+	]);
 };
