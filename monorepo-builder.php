5c9377c592f5ea3ac46a7f511c60657a50aa0066
Evan Hildreth <me@eph.me>
2025-02-01 21:37:16 -0500

    prepare release

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -2,8 +2,15 @@
 
 declare(strict_types=1);
 
-use Symplify\MonorepoBuilder\ComposerJsonManipulator\ValueObject\ComposerJsonSection;
 use Symplify\MonorepoBuilder\Config\MBConfig;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\AddTagToChangelogReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushNextDevReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushTagReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetCurrentMutualDependenciesReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetNextMutualDependenciesReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\TagVersionReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateBranchAliasReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateReplaceReleaseWorker;
 
 return static function (MBConfig $mbConfig): void {
 	$mbConfig->packageDirectories([
@@ -27,5 +34,17 @@ return static function (MBConfig $mbConfig): void {
 			"lintfix" => "./vendor/squizlabs/php_codesniffer/bin/phpcbf",
 			"monorepo" => "./vendor/bin/monorepo-builder",
 		]
+	]);
+
+	// Release workers - in order of execution.
+	$mbConfig->workers([
+		UpdateReplaceReleaseWorker::class,
+		SetCurrentMutualDependenciesReleaseWorker::class,
+		AddTagToChangelogReleaseWorker::class,
+		TagVersionReleaseWorker::class,
+		PushTagReleaseWorker::class,
+		SetNextMutualDependenciesReleaseWorker::class,
+		UpdateBranchAliasReleaseWorker::class,
+		PushNextDevReleaseWorker::class,
 	]);
 };
