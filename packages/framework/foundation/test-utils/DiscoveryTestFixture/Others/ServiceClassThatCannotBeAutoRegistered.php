e7fa511be4e439b9644f5506a326beac81448c59
Evan Hildreth <me@eph.me>
2025-08-10 16:53:02 -0400

    Add discovered service that can't be auto-registered Working on the best way to fix this, but hereis the failing test.

---
+++ packages/framework/foundation/test-utils/DiscoveryTestFixture/Others/ServiceClassThatCannotBeAutoRegistered.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Smolblog\Foundation\Test\DiscoveryTestFixture\Others;
+
+use Smolblog\Foundation\Service;
+use Smolblog\Foundation\Test\DiscoveryTestFixture\SameDirectoryService;
+use Smolblog\Foundation\Test\DiscoveryTestFixture\SomeFolder\{SomeAbstractServiceInterfaceClass, SomeInterface};
+
+final class ServiceClassThatCannotBeAutoRegistered implements Service {
+	public function __construct(
+		private SomeInterface $one,
+		private SomeAbstractServiceInterfaceClass $two,
+		private SameDirectoryService $three,
+		private string $dsn,
+	) {
+	}
+}
